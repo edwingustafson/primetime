fd35ceff15c5e8d47ee952eb7a58f2224582a267
Edwin Gustafson <edwingustafson@gmail.com>
2022-07-20 11:36:28 -0400

    Java 17

--- src/test/java/com/edwingustafson/primetime/PrimeTimeTest.java
+++ src/test/java/com/edwingustafson/primetime/PrimeTimeTest.java
@@ -1,32 +1,45 @@
 package com.edwingustafson.primetime;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Random;
 
+@DisplayName("PrimeTime")
 public class PrimeTimeTest {
-    final int[] ns = new int[2_097_152];
-    final Random random = new Random();
+	final int[] ns = new int[500_000];
+	final Random random = new Random();
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        Arrays.setAll(ns, (i) -> 2 + random.nextInt(9998));
-    }
+	@BeforeEach
+	public void setUp() throws Exception {
+		Arrays.setAll(ns, (i) -> 2 + random.nextInt(9998));
+	}
 
-    @Test
-    public void imperative() {
-        for(int i = 0; i < ns.length; i++) PrimeTime.prime(ns[i]);
-    }
+	@Test
+	@DisplayName("Serial loop")
+	public void loop() {
+		for (int i = 0; i < ns.length; i++)
+			PrimeTime.prime(ns[i]);
+	}
 
-    @Test
-    public void functional() {
-        Arrays.stream(ns).forEach(PrimeTime::prime);
-    }
+	@Test
+	@DisplayName("Serial iterator")
+	public void iterator() {
+		for (int n: ns)
+			PrimeTime.prime(n);
+	}
 
-    @Test
-    public void parallel() {
-        Arrays.stream(ns).parallel().forEach(PrimeTime::prime);
-    }
+	@Test
+	@DisplayName("Serial stream")
+	public void functional() {
+		Arrays.stream(ns).forEach(PrimeTime::prime);
+	}
+
+	@Test()
+	@DisplayName("Parallel stream")
+	public void parallel() {
+		Arrays.stream(ns).parallel().forEach(PrimeTime::prime);
+	}
 }
